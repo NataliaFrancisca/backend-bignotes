5429e191a11c4811b24613a9b3baf6a975ac5b6b
nataliafrancisca <nathifrancisca@gmail.com>
2026-07-11 22:00:27 -0300

    feat: organize class Main and add more examples

--- fundamentos/tiposDados/Main.java
+++ fundamentos/tiposDados/Main.java
@@ -2,17 +2,41 @@ package fundamentos.tiposDados;
 
 public class Main {
     public static void main(String[] args) {
-        TiposPrimitivos primitivos = new TiposPrimitivos();
-
+        // principais tipos primitivos
         int age = 26;
         double price = 20.90;
         boolean isStudent = true;
         char courseClass = 'A';
 
+        // INTEGER
+        byte phoneB = 123;
+        short phoneS = 12345;
+        int phoneI = 1234567890;
+        long phoneL = 12345678998765432L;
+
+        // FLUTUANTE
+        float priceF = 120.30f;
+        priceF = 1200.20f;
+        priceF = 20000.30f;
+        System.out.println("price f: " + priceF);
+
+        double priceD = 1.50;
+        priceD = 20.30;
+        priceD = 50000.50;
+        System.out.println("price d: " + priceD);
+
+        double priceFloatAsDouble = (double) priceF;
+        System.out.println("priceFloatAsDouble: " + priceFloatAsDouble);
+
+        priceD = priceD * Double.MAX_VALUE;
+        System.out.println("priceD: " + priceD);
+
+        // tipos primitivos usando tratamento de erros.
+        TiposPrimitivos primitivos = new TiposPrimitivos();
+
         primitivos.askForAge();
         primitivos.askForPrice();
         primitivos.askIfIsStudent();
         primitivos.askWhichClassIs();
-
     }
 }
